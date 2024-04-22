f4896d622a5df144f4c55feb3d466bb474c47460
Matthieu Mota <matthieumota@gmail.com>
2024-04-22 19:43:18 +0200

    Begin Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+export class Pizza {
+  id: number;
+  name: string;
+  price: number;
+
+  constructor(id: number, name: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const PIZZAS: Pizza[] = [
+  { id: 1, name: 'Reine', price: 12 },
+  { id: 2, name: '4 fromages', price: 13 },
+  { id: 3, name: 'Orientale', price: 11 },
+  { id: 4, name: 'Cannibale', price: 9 }
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title: string = 'pizzaparty';
+  selectedPizza!: Pizza;
+  pizzas: Pizza[] = PIZZAS;
+
+  onSelect(pizza: Pizza): void {
+    this.selectedPizza = pizza;
+  }
 }
