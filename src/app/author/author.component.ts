1e26798471e2c34b77bc828de11f82e186633d05
Matthieu Mota <matthieumota@gmail.com>
2024-04-23 11:40:18 +0200

    Fix author

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -1,21 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../models/user';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-author',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   template: `
-    <div class="card" [ngStyle]="{ width: '200px' }">
-      <img class="card-img-top" [src]="author.avatar" [alt]="author.firstname + ' ' + author.name">
+    <div class="card my-4" [ngStyle]="{ width: '200px' }">
+      <img *ngIf="show" class="card-img-top" [src]="author.avatar" [alt]="author.firstname + ' ' + author.name">
       <div class="card-body">
         <h5 class="card-title">{{ author.firstname + ' ' + author.name }}</h5>
         <p class="card-text">{{ author.age }} ans</p>
+        <label>
+          <input type="checkbox" [(ngModel)]="show">
+          <ng-container *ngIf="!show; else other"> Afficher avatar</ng-container>
+          <ng-template #other> Cacher avatar</ng-template>
+        </label>
       </div>
     </div>
   `
 })
 export class AuthorComponent {
   @Input() author!: User;
+  show: boolean = true;
 }
