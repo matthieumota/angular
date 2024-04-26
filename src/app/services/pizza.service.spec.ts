450a49c4f72958cc7797bd6677b78af00ce802db
Matthieu Mota <matthieumota@gmail.com>
2024-04-26 17:48:49 +0200

    Fix tests

--- src/app/services/pizza.service.spec.ts
+++ src/app/services/pizza.service.spec.ts
@@ -1,8 +1,15 @@
-import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, fakeAsync, flush, flushMicrotasks, tick, waitForAsync } from '@angular/core/testing';
 import { PizzaService } from './pizza.service';
-import { PIZZAS } from '../http/fake.interceptor';
-import { Observable, delay } from 'rxjs';
+import { PIZZAS, fakeInterceptor } from '../http/fake.interceptor';
+import { Observable, delay, of } from 'rxjs';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+class FakeInterceptorClass implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return fakeInterceptor(req, next.handle);
+  }
+}
 
 describe('PizzaService', () => {
   let service: PizzaService;
@@ -10,24 +17,34 @@ describe('PizzaService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ]
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        // { provide: HTTP_INTERCEPTORS, useClass: FakeInterceptorClass, multi: true }
+      ]
     });
     service = TestBed.inject(PizzaService);
     http = TestBed.inject(HttpTestingController);
   });
 
   it('should return a list of pizzas', fakeAsync(() => {
-    /*.pipe(delay(500))*/
-    service.getPizzas().subscribe(pizzas => {
-      expect(pizzas).toBe(PIZZAS);
+    service.getPizzas().pipe(delay(500)).subscribe((v) => {
+      expect(v).toEqual(PIZZAS);
     });
-
-    tick(500); // Nécessaire pour terminer une tâche async (promesse, setTimeout, delay observable)
 
     // Ici, on mock la requête (simuler une fausse requête avec de fausses données)
     const req = http.expectOne('https://monapi.com/api/pizzas');
     expect(req.request.method).toBe('GET');
     req.flush(PIZZAS);
     http.verify();
+
+    // Le tick doit ABSOLUMENT être après le flush... pour passer le delay(500)
+    tick(500);
+  }));
+
+  // Ici, on peut utiliser waitForAsync pour utiliser l'interceptor
+  it('should return a list of pizzas', waitForAsync(() => {
+    service.getPizzas().subscribe((v) => {
+      expect(v).toEqual(PIZZAS);
+    });
   }));
 });
