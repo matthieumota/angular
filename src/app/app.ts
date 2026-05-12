c036bf1029438ebfb347ad8e83e39d78e162ef49
Matthieu Mota <matthieumota@gmail.com>
2026-05-12 12:28:22 +0200

    begin service and show promise

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Pizza } from './models/pizza';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -6,13 +6,7 @@ import { PizzaSelected } from './components/pizza-selected/pizza-selected';
 import { Counter } from './components/counter/counter';
 import { Author } from './components/author/author';
 import { User } from './models/user';
-
-const PIZZAS: Pizza[] = [
-  { id: 1, name: 'Reine', price: 12, image: '/assets/pizzas/reine.jpg' },
-  { id: 2, name: '4 fromages', price: 13, image: '/assets/pizzas/4-fromages.jpg' },
-  { id: 3, name: 'Orientale', price: 11, image: '/assets/pizzas/orientale.jpg' },
-  { id: 4, name: 'Cannibale', price: 9, image: '/assets/pizzas/cannibale.jpg' }
-];
+import { PizzaRepository } from './services/pizza-repository';
 
 @Component({
   selector: 'app-root',
@@ -25,12 +19,18 @@ export class App implements OnInit {
   protected pizza: Pizza = new Pizza(1, 'Reine', 12, '/assets/pizzas/reine.jpg');
   // Utilisation d'un signal pour la pizza sélectionnée (et renommage de la variable pour éviter les conflits)
   protected selectedPizza = signal<Pizza | null>(null);
-  protected pizzas: Array<Pizza> | Pizza[] = PIZZAS;
+  protected pizzas: Array<Pizza> | Pizza[] = [];
 
   protected withoutSignal: number = 0;
   protected withSignal = signal(0);
 
-  ngOnInit(): void {
+  pizzaRepository = inject(PizzaRepository);
+
+  ngOnInit() {
+    this.pizzaRepository.getPizzas().then(pizzas => {
+      this.pizzas = pizzas;
+    });
+
     setInterval(() => {
       this.withoutSignal++;
       this.withSignal.update(n => n + 1);
