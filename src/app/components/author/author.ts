46bcf675b9f7bd343fe35f3368c9d7df04f267de
Matthieu Mota <matthieumota@gmail.com>
2026-05-12 10:37:50 +0200

    computed

--- src/app/components/author/author.ts
+++ src/app/components/author/author.ts
@@ -1,32 +1,35 @@
-import { Component, input, signal, linkedSignal } from '@angular/core';
+import { Component, input, signal, linkedSignal, computed } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-author',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './author.html',
 })
 export class Author {
   user = input.required<User>();
   protected birthYear = linkedSignal(() => {
-    return parseInt(this.user().birthDate.split('-')[0]);
-  });
-  protected showAvatar = signal(true);
+    return new Date(this.user().birthDate).getFullYear();
+  })
   protected years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
 
-  protected age(): number {
+  protected age = computed(() => {
+    console.log('Calculating age...');
     const today = new Date();
     const birth = new Date(this.user().birthDate);
     let age = today.getFullYear() - this.birthYear();
     if (today.getMonth() < birth.getMonth() || (today.getMonth() === birth.getMonth() && today.getDate() < birth.getDate())) age--;
     return age;
-  }
+  });
 
-  protected isMajor(): boolean {
+  protected isMajor = computed(() => {
+    console.log('Calculating if major...');
     return this.age() >= 18;
-  }
+  });
 
+  protected showAvatar = signal(true);
   toggleAvatar(): void {
     this.showAvatar.update(v => !v);
   }
