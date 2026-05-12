65dd580372239ff8dbd1b4e9687cedb03ee9d903
Matthieu Mota <matthieumota@gmail.com>
2026-05-12 09:36:10 +0200

    counter

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -1,22 +1,24 @@
-import { Component, input, linkedSignal } from '@angular/core';
+import { Component, input, linkedSignal, model } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.html',
 })
 export class Counter {
-  value = input<number>(0);
-  protected count = linkedSignal(this.value);
+  value = model(0);
+
+  initialValue = input(0);
+  realValue = linkedSignal(() => this.initialValue());
 
   decrement(): void {
-    if (this.count() > 0) {
-      this.count.set(this.count() - 1);
+    if (this.realValue() > 0) {
+      this.realValue.set(this.realValue() - 1);
     }
   }
 
   increment(): void {
-    if (this.count() < 10) {
-      this.count.update(n => n + 1);
+    if (this.realValue() < 10) {
+      this.realValue.update(old => old + 1);
     }
   }
 }
